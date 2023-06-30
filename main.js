c3381650cb40af5767e2139b72f486e9239f9c6f
shalinispk7 <shalinispk7@gmail.com>
2023-06-30 18:01:22 +0530

    hamburger added

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("acTive");
+    navMenu.classList.toggle("acTive");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("acTive");
+    navMenu.classList.remove("acTive");
+}
